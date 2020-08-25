3fc9852ca9e2ee3f8f6a39d3c4ca0d5cbe1ef9c5
Grace <gminnis1@yahoo.com>
2020-08-25 13:55:52 -0500

    Finished class 3 Studio restaurant project

---
+++ src/com/company/MenuItem.java
@@ -0,0 +1,45 @@
+package com.company;
+
+public class MenuItem {
+
+    private String name;
+    private double price;
+    private String description;
+    private String category;
+
+    public MenuItem (String name, double price, String description, String category) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+}
